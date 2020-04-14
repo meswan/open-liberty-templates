dc9f051c54d06c528e5808c888bad0802837fa67
meswan <meswan@ca.ibm.com>
2020-04-14 15:01:32 -0400

    upgraded plugin,dependency v, new ready logic

--- src/main/java/io/openliberty/health/BookReadinessCheck.java
+++ src/main/java/io/openliberty/health/BookReadinessCheck.java
@@ -11,24 +11,58 @@
 
 package io.openliberty.health;
 
-import io.openliberty.rest.BookResource;
-
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.client.WebTarget;
+
 import org.eclipse.microprofile.health.Readiness;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import io.openliberty.rest.BookResource;
 
 @Readiness
 @ApplicationScoped
 public class BookReadinessCheck implements HealthCheck {
+
+  @Inject
+  @ConfigProperty(name = "io_openliberty_sample_rest_inMaintenance")
+  private boolean isInMaintenance;
+  
+  public boolean isHealthy() {
+    if (isInMaintenance) {
+      return false;
+    }
+    try {
+      Client client = ClientBuilder.newClient();
+      WebTarget target = client.target("http://192.168.0.16:9080/RestServer/v1/books");
+      Response response = target.request(MediaType.APPLICATION_JSON).get();
+      
+      if (response.getStatus() != 200) {
+        return false;
+      }
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
   @Override
   public HealthCheckResponse call() {
-    if (!System.getProperty("wlp.server.name").equals("defaultServer")) {
+
+    if (!isHealthy()) {
       return HealthCheckResponse.named(BookResource.class.getSimpleName() + "Readiness")
-                                .withData("default server", "not available").down()
+                                .withData("Services", "not available").down()
                                 .build();
     }
+  
     return HealthCheckResponse.named(BookResource.class.getSimpleName() + "Readiness")
-                              .withData("default server", "available").up().build();
+                              .withData("Services", "available").up().build();
   }
+
 }
